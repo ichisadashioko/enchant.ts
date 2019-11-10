cf57dc9bed52717c2fe5f6eb67bbe0e4477b970d
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-10 08:25:49 -0500

    :fire: remove unrelated stuffs

--- original-enchant.js/tasks/build.coffee
+++
@@ -1,4 +0,0 @@
-module.exports = (grunt) ->
-
-  grunt.registerTask 'build', 'Create build directory.', ->
-    grunt.task.run 'copy:lang/en/:build/', 'exec:whatever:git checkout build/readme.md'
--- original-enchant.js/tests/mocha/enchant.js/test_enchant.js
+++
@@ -1,9 +0,0 @@
-describe("enchant.js", function(){
-    afterEach(function(){
-    });
-
-    describe("export", function(){
-        it("should export globally", function(){
-        });
-    });
-});
