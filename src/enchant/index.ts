8ef9dc57929398b9787d6346a247da72e4f04647
Ichisada Shioko <ichisadashioko@gmail.com>
2020-03-11 15:56:25 +0700

    :bug: copy ActionEventTarget.js

--- src/enchant/index.ts
+++ src/enchant/index.ts
@@ -2804,8 +2804,33 @@ namespace enchant {
 
     }
 
-    export class ActionEventTarget {
+    /**
+     * EventTarget which can change the context of event listeners.
+     */
+    export class ActionEventTarget extends EventTarget {
 
+        constructor() {
+            super()
+        }
+
+        dispatchEvent(e) {
+            let target = this.node ? this.node : this
+
+            e.target = target
+            e.localX = e.x - target._offsetX
+            e.localY = e.y - target._offsetY
+
+            if (this['on' + e.type] != null) {
+                this['on' + e.type].call(target, e)
+            }
+            let listeners = this._listeners[e.type]
+            if (listeners != null) {
+                listeners = listeners.slice()
+                for (let i = 0; i < listeners.length; i++) {
+                    listeners[i].call(target, e)
+                }
+            }
+        }
     }
 
     /**
