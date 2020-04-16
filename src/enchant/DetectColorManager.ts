c2156be3db9a2b1579b0758f239405f7c2c558f2
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-17 03:20:21 +0700

    :white_check_mark: end DetectColorManager.initialize

--- src/enchant/DetectColorManager.ts
+++ src/enchant/DetectColorManager.ts
@@ -1,2 +1,17 @@
 
-export default class DetectColorManager { }
+export default class DetectColorManager {
+    reference: Array<any>
+    colorResolution: number
+    max: number
+    capacity: number
+
+    constructor(reso = 16, max = 1) {
+        this.reference = []
+        this.colorResolution = reso
+        this.max = max
+        this.capacity = Math.pow(this.colorResolution, 3)
+        for (let i = 1; i < this.capacity; i++) {
+            this.reference[i] = null
+        }
+    }
+}
