2452331b48af6ed63cb37de5caf98e3aab735bb3
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-21 12:05:31 +0900

    resolve modules for KeyboardInputSource.ts

--- src/enchant/KeyboardInputSource.ts
+++ src/enchant/KeyboardInputSource.ts
@@ -1,10 +1,11 @@
+import BinaryInputSource from './BinaryInputSource'
 
 export default class KeyboardInputSource extends BinaryInputSource {
     static _instances = {};
 
     static getByKeyCode(keyCode: string) {
         if (!this._instances[keyCode]) {
-            this._instances[keyCode] = new enchant.KeyboardInputSource(keyCode);
+            this._instances[keyCode] = new KeyboardInputSource(keyCode);
         }
         return this._instances[keyCode];
     }
