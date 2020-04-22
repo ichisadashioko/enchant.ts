7b870ace5a5976b4ad18d720aa13836981a1797d
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-22 22:15:07 +0700

    LoadingScene: start converting - halted

--- src/enchant/LoadingScene.ts
+++ src/enchant/LoadingScene.ts
@@ -1,4 +1,14 @@
+import Scene from './Scene'
+import Sprite from './Sprite'
 
-export default class LoadingScene {
+export default class LoadingScene extends Scene {
+    constructor() {
+        super()
 
+        this.backgroundColor = '#000'
+        let barWidth = this.width * 0.4 | 0
+        let barHeight = this.width * 0.05 | 0
+        let border = barWidth * 0.03 | 0
+        let bar = new Sprite(barWidth, barHeight)
+    }
 }
