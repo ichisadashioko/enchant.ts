233eb1e88f7e8ca9a656cc7279b4663bc00af082
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-22 22:16:51 +0700

    Scene: improve typing, remove semicolons

--- src/enchant/Scene.ts
+++ src/enchant/Scene.ts
@@ -4,9 +4,6 @@ import Event from './Event'
 import EventType from './EventType'
 import Node from './Node'
 
-interface NodeMap {
-    [type: string]: Node;
-}
 
 /**
  * Class that becomes the root of the display object tree.
@@ -19,15 +16,15 @@ interface NodeMap {
  * Scenes are drawn in the order that they are added.
  * 
  * @example
- * var scene = new Scene();
- * scene.addChild(player);
- * scene.addChild(enemy);
- * core.pushScene(scene);
+ * var scene = new Scene()
+ * scene.addChild(player)
+ * scene.addChild(enemy)
+ * core.pushScene(scene)
  */
 export default class Scene extends Group {
 
     _element: HTMLElement
-    _layers: NodeMap
+    _layers: Record<string, Node>
     _layerPriority: []
 
     _backgroundColor: string
@@ -37,152 +34,153 @@ export default class Scene extends Group {
     }
 
     set backgroundColor(color: string) {
-        this._backgroundColor = this._element.style.backgroundColor = color;
+        this._backgroundColor = this._element.style.backgroundColor = color
     }
 
     constructor() {
-        super();
+        super()
 
-        let core = Core.instance;
+        let core = Core.instance
 
         // All nodes (entities, groups, scenes) have reference to 
         // the scene that it belongs to.
-        this.scene = this;
-        this._backgroundColor = null;
+        this.scene = this
+        this._backgroundColor = null
 
         // Create div tag which prossesses its layers
-        this._element = document.createElement('div');
-        this._element.style.position = 'absolute';
-        this._element.style.overflow = 'hidden';
-        this._element.style.transformOrigin = '0 0';
+        this._element = document.createElement('div')
+        this._element.style.position = 'absolute'
+        this._element.style.overflow = 'hidden'
+        this._element.style.transformOrigin = '0 0'
 
-        this._layers = {};
-        this._layerPriority = [];
+        this._layers = {}
+        this._layerPriority = []
 
-        this.addEventListener(EventType.CHILD_ADDED, this._onchildadded);
-        this.addEventListener(EventType.CHILD_REMOVED, this._onchildremoved);
-        this.addEventListener(EventType.ENTER, this._onenter);
-        this.addEventListener(EventType.EXIT, this._onexit);
+        this.addEventListener(EventType.CHILD_ADDED, this._onchildadded)
+        this.addEventListener(EventType.CHILD_REMOVED, this._onchildremoved)
+        this.addEventListener(EventType.ENTER, this._onenter)
+        this.addEventListener(EventType.EXIT, this._onexit)
 
-        this.addEventListener(EventType.CORE_RESIZE, this._oncoreresize);
+        this._dispatchExitframe = this._dispatchExitframe.bind(this)
 
-        this._oncoreresize(core);
+        this.addEventListener(EventType.CORE_RESIZE, this._oncoreresize)
+
+        this._oncoreresize(core)
     }
 
     _dispatchExitframe() {
-        let layer;
         for (let prop in this._layers) {
-            layer = this._layers[prop];
-            layer.dispatchEvent(new Event(EventType.EXIT_FRAME));
+            let layer = this._layers[prop]
+            layer.dispatchEvent(new Event(EventType.EXIT_FRAME))
         }
     }
 
     set x(x: number) {
-        this._x = x;
+        this._x = x
         for (let type in this._layers) {
-            this._layers[type].x = x;
+            this._layers[type].x = x
         }
     }
 
     set y(y: number) {
-        this._y = y;
+        this._y = y
         for (let type in this._layers) {
-            this._layers[type].y = y;
+            this._layers[type].y = y
         }
     }
 
     set width(width: number) {
-        this._width = width;
+        this._width = width
         for (let type in this._layers) {
-            this._layers[type].width = width;
+            this._layers[type].width = width
         }
     }
 
     set height(height: number) {
-        this._height = height;
+        this._height = height
         for (let type in this._layers) {
-            this._layers[type].height = height;
+            this._layers[type].height = height
         }
     }
 
     set rotation(rotation: number) {
-        this._rotation = rotation;
+        this._rotation = rotation
         for (let type in this._layers) {
-            this._layers[type].rotation = rotation;
+            this._layers[type].rotation = rotation
         }
     }
 
     set scaleX(scale: number) {
-        this._scaleX = scale;
+        this._scaleX = scale
         for (let type in this._layers) {
-            this._layers[type].scaleX = scale;
+            this._layers[type].scaleX = scale
         }
     }
 
     set scaleY(scale: number) {
-        this._scaleY = scale;
+        this._scaleY = scale
         for (let type in this._layers) {
-            this._layers[type].scaleY = scale;
+            this._layers[type].scaleY = scale
         }
     }
 
     remove() {
-        this.clearEventListener();
+        this.clearEventListener()
         while (this.childNodes.length > 0) {
-            this.childNodes[0].remove();
+            this.childNodes[0].remove()
         }
 
-        return Core.instance.removeScene(this);
+        return Core.instance.removeScene(this)
     }
 
     _oncoreresize(e: Core) {
-        this._element.style.width = e.width + 'px';
-        this.width = e.width;
-        this._element.style.height = e.height + 'px';
-        this.height = e.height;
-        this._element.style.transform = `scale(${e.scale})`;
+        this._element.style.width = e.width + 'px'
+        this.width = e.width
+        this._element.style.height = e.height + 'px'
+        this.height = e.height
+        this._element.style.transform = `scale(${e.scale})`
 
         for (let type in this._layers) {
-            this._layers[type].dispatchEvent(e);
+            this._layers[type].dispatchEvent(e)
         }
     }
 
     _onchildadded(e: Event) {
-        let child = e.node;
-        let next = e.next;
-        let target: string, i: number;
+        let child = e.node
+        let next = e.next
+        let target: string, i: number
         if (child._element) {
-            target = 'Dom';
-            i = 1;
+            target = 'Dom'
+            i = 1
         } else {
-            target = 'Canvas';
-            i = 0;
+            target = 'Canvas'
+            i = 0
         }
         if (!this._layers[target]) {
-            this.addLayer(target, i);
+            this.addLayer(target, i)
         }
-        child._layer = this._layers[target];
-        this._layers[target].insertBefore(child, next);
-        child.parentNode = this;
+        child._layer = this._layers[target]
+        this._layers[target].insertBefore(child, next)
+        child.parentNode = this
     }
 
     _onchildremoved(e: Event) {
-        let child = e.node;
-        child._layer.removeChild(child);
-        child._layer = null;
+        let child = e.node
+        child._layer.removeChild(child)
+        child._layer = null
     }
 
     _onenter() {
         for (let type in this._layers) {
-            this._layers[type]._startRendering();
+            this._layers[type]._startRendering()
         }
-        Core.instance.addEventListener('exitframe', this._dispatchExitframe);
+        Core.instance.addEventListener('exitframe', this._dispatchExitframe)
     }
 
     _onexit() {
         for (let type in this._layers) {
-            this._layers[type]._stopRendering();
+            this._layers[type]._stopRendering()
         }
-        Core.instance.removeEventListener('exitframe', this._dispatchExitframe);
+        Core.instance.removeEventListener('exitframe', this._dispatchExitframe)
     }
 }
