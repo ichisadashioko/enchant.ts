0c95c6f277936807aa4fd551eb74bd96d9be9f0a
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-01 08:33:57 +0700

    Surface: remove semicolons

--- src/enchant/Surface.ts
+++ src/enchant/Surface.ts
@@ -13,35 +13,35 @@ import Event from './Event'
  * 
  * @example
  * // Creates Sprite that displays a circle.
- * var ball = new Sprite(50, 50);
- * var surface = new Surface(50, 50);
- * surface.context.beginPath();
- * surface.context.arc(25, 25, 25, 0, Math.PI*2, true);
- * surface.context.fill();
- * ball.image = surface;
+ * var ball = new Sprite(50, 50)
+ * var surface = new Surface(50, 50)
+ * surface.context.beginPath()
+ * surface.context.arc(25, 25, 25, 0, Math.PI*2, true)
+ * surface.context.fill()
+ * ball.image = surface
  */
 export default abstract class Surface extends EventTarget {
 
     /**
      * Surface width.
      */
-    width: number;
+    width: number
 
     /**
      * Surface height.
      */
-    height: number;
+    height: number
 
-    _element: HTMLCanvasElement | HTMLImageElement;
-    _pattern?: CanvasPattern;
+    _element: HTMLCanvasElement | HTMLImageElement
+    _pattern?: CanvasPattern
 
     /**
      * The copied Surface.
      */
     clone() {
-        let clone = new CanvasSurface(this.width, this.height);
-        clone.draw(this);
-        return clone;
+        let clone = new CanvasSurface(this.width, this.height)
+        clone.draw(this)
+        return clone
     }
 
     /**
@@ -49,7 +49,7 @@ export default abstract class Surface extends EventTarget {
      * @return The data URI schema that identifies this Surface 
      * and can be used to include this Surface into a DOM tree.
      */
-    abstract toDataURL(): string;
+    abstract toDataURL(): string
 
     /**
      * Loads an image and creates a Surface object out of it.
@@ -70,35 +70,35 @@ export default abstract class Surface extends EventTarget {
      * @param onerror on error callback.
      */
     static load(src: string, callback: Function, onerror?: Function): ImageSurface {
-        let image = new Image();
-        let surface = new ImageSurface(image, `url(${src})`);
+        let image = new Image()
+        let surface = new ImageSurface(image, `url(${src})`)
 
-        onerror = onerror || function () { };
-        surface.addEventListener('load', callback);
-        surface.addEventListener('error', onerror);
+        onerror = onerror || function () { }
+        surface.addEventListener('load', callback)
+        surface.addEventListener('error', onerror)
         image.onerror = function () {
-            let e = new Event(EventType.ERROR);
-            e.message = `Cannot load an assets: ${image.src}`;
-            Core.instance.dispatchEvent(e);
-            surface.dispatchEvent(e);
-        };
+            let e = new Event(EventType.ERROR)
+            e.message = `Cannot load an assets: ${image.src}`
+            Core.instance.dispatchEvent(e)
+            surface.dispatchEvent(e)
+        }
 
         image.onload = function () {
-            surface.width = image.width;
-            surface.height = image.height;
-            surface.dispatchEvent(new Event(EventType.LOAD));
-        };
+            surface.width = image.width
+            surface.height = image.height
+            surface.dispatchEvent(new Event(EventType.LOAD))
+        }
 
-        image.src = src;
-        return surface;
+        image.src = src
+        return surface
     }
 
-    static _staticCanvas2DContext = document.createElement('canvas').getContext('2d');
+    static _staticCanvas2DContext = document.createElement('canvas').getContext('2d')
 
     static _getPattern(surface: Surface, force: boolean) {
         if (!surface._pattern || force) {
-            surface._pattern = CanvasSurface._staticCanvas2DContext.createPattern(surface._element, 'repeat');
+            surface._pattern = CanvasSurface._staticCanvas2DContext.createPattern(surface._element, 'repeat')
         }
-        return surface._pattern;
+        return surface._pattern
     }
 }
