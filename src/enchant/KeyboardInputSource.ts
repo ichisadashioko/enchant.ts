4aea8f285208f0937c2be551d43edf5c19fc5c1c
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-01 08:34:34 +0700

    KeyboardInputSource: improve typing
    - remove semicolons

--- src/enchant/KeyboardInputSource.ts
+++ src/enchant/KeyboardInputSource.ts
@@ -1,16 +1,16 @@
 import BinaryInputSource from './BinaryInputSource'
 
 export default class KeyboardInputSource extends BinaryInputSource {
-    static _instances = {};
+    static _instances: Record<string, KeyboardInputSource> = {}
 
     static getByKeyCode(keyCode: string) {
-        if (!this._instances[keyCode]) {
-            this._instances[keyCode] = new KeyboardInputSource(keyCode);
+        if (!KeyboardInputSource._instances[keyCode]) {
+            KeyboardInputSource._instances[keyCode] = new KeyboardInputSource(keyCode)
         }
-        return this._instances[keyCode];
+        return this._instances[keyCode]
     }
 
     constructor(keyCode: string) {
-        super(keyCode);
+        super(keyCode)
     }
 }
