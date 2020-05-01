ff7e34f6006e736bc7a45c10ef651435e1ee6c9d
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-01 08:35:48 +0700

    ImageSurface: remove semicolons

--- src/enchant/ImageSurface.ts
+++ src/enchant/ImageSurface.ts
@@ -1,21 +1,21 @@
 import Surface from './Surface'
 
 export default class ImageSurface extends Surface {
-    _element: HTMLImageElement;
-    _css: string;
+    _element: HTMLImageElement
+    _css: string
 
     constructor(element, css) {
-        super();
-        this._element = element;
-        this._css = css;
+        super()
+        this._element = element
+        this._css = css
     }
 
     toDataURL() {
-        let src = this._element.src;
+        let src = this._element.src
         if (src.slice(0, 5) === 'data:') {
-            return src;
+            return src
         } else {
-            return this.clone().toDataURL();
+            return this.clone().toDataURL()
         }
     }
 }
