9e815e45b7ab030c20b944f5bfd0d3004c926878
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-16 16:16:00 +0700

    DomlessManager: add partial implementation

--- src/enchant/DomlessManager.ts
+++ src/enchant/DomlessManager.ts
@@ -1,1 +1,18 @@
-export default class DomlessManager { }
+import Node from './Node'
+
+
+/**
+ * TODO Most of the code for DomlessManager are dead code from examples
+ * inspection so there is not example usages. There is some from the
+ * integration tests.
+ */
+export default class DomlessManager {
+
+    _domRef: [] // TODO
+    targetNode: Node
+
+    constructor(node: Node) {
+        this._domRef = []
+        this.targetNode = node
+    }
+}
