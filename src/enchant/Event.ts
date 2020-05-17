8c1b96b135e316be3b983f24ecf402340637942e
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-17 18:07:35 +0700

    Event: update jsdoc

--- src/enchant/Event.ts
+++ src/enchant/Event.ts
@@ -100,7 +100,8 @@ export default class Event {
      * 
      * When preloading images, it is necessary to wait until preloading is complete
      * before starting the game.
-     * Issued by: `enchant.Core`
+     * 
+     * Issued by: {@link enchant.Core}
      * 
      * @example
      * var core = new Core(320, 320);
@@ -114,84 +115,85 @@ export default class Event {
 
     /**
      * An event dispatched when an error occurs.
-     * Issued by `enchant.Core`, `enchant.Surface`, `enchant.WebAudioSound`, `enchant.DOMSound`
+     * 
+     * Issued by {@link enchant.Core}, {@link enchant.Surface}, {@link enchant.WebAudioSound}, {@link enchant.DOMSound}
      */
     static ERROR = 'error'
 
     /**
      * An event dispatched when the display size is changed.
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static CORE_RESIZE = 'coreresize'
 
     /**
      * An event dispatched while the core is loading.
      * Dispatched each time an image is preloaded.
-     * Issued by: `enchant.LoadingScene`
+     * Issued by: {@link enchant.LoadingScene}
      */
     static PROGRESS = 'progress'
 
     /**
      * An event which is occurring when a new frame is being processed.
-     * Issued object: `enchant.Core`, `enchant.Node`
+     * Issued object: {@link enchant.Core}, {@link enchant.Node}
      */
     static ENTER_FRAME = 'enterframe'
 
     /**
      * An event dispatched at the end of processing a new frame.
-     * Issued by: `enchant.Core`, `enchant.Node`
+     * Issued by: {@link enchant.Core}, {@link enchant.Node}
      */
     static EXIT_FRAME = 'exitframe'
 
     /**
      * An event dispatched when a Scene begins.
-     * Issued by: `enchant.Scene`
+     * Issued by: {@link enchant.Scene}
      */
     static ENTER = 'enter'
 
     /**
      * An event dispatched when a Scene ends.
-     * Issued by: `enchant.Scene`
+     * Issued by: {@link enchant.Scene}
      */
     static EXIT = 'exit'
 
     /**
      * An event dispatched when a Child is added to a Node.
-     * Issued by: `enchant.Group`, `enchant.Scene`
+     * Issued by: {@link enchant.Group}, {@link enchant.Scene}
      */
     static CHILD_ADDED = 'childadded'
 
     /**
      * An event dispatched when a Node is added to a Group.
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static ADDED = 'added'
 
     /**
      * An event dispatched when a Node is added to a Scene.
      * 
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static ADDED_TO_SCENE = 'addedtoscene'
 
     /**
      * An event dispatched when a Child is removed from a Node.
      * 
-     * Issued by: `enchant.Group`, `enchant.Scene`
+     * Issued by: {@link enchant.Group}, {@link enchant.Scene}
      */
     static CHILD_REMOVED = 'childremoved'
 
     /**
      * An event dispatched when a Node is deleted from a Group.
      * 
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static REMOVED = 'removed'
 
     /**
      * An event dispatched when a Node is deleted from a Scene.
      * 
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static REMOVED_FROM_SCENE = 'removedfromscene'
 
@@ -199,7 +201,7 @@ export default class Event {
      * An event dispatched when a touch event intersecting a Node begins.
      * A mouse event counts as a touch event. 
      * 
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static TOUCH_START = 'touchstart'
 
@@ -207,7 +209,7 @@ export default class Event {
      * An event dispatched when a touch event intersecting the Node has been moved.
      * A mouse event counts as a touch event. 
      * 
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static TOUCH_MOVE = 'touchmove'
 
@@ -215,126 +217,126 @@ export default class Event {
      * An event dispatched when a touch event intersecting the Node ends.
      * A mouse event counts as a touch event. 
      * 
-     * Issued by: `enchant.Node`
+     * Issued by: {@link enchant.Node}
      */
     static TOUCH_END = 'touchend'
 
     /**
      * An event dispatched when an Entity is rendered.
      * 
-     * Issued by: `enchant.Entity`
+     * Issued by: {@link enchant.Entity}
      */
     static RENDER = 'render'
 
     /**
      * An event dispatched when a button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static INPUT_START = 'inputstart'
 
     /**
      * An event dispatched when button inputs change.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static INPUT_CHANGE = 'inputchange'
 
     /**
      * An event dispatched when button input ends.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static INPUT_END = 'inputend'
 
     /**
      * An internal event which is occurring when a input changes.
      * 
-     * Issued object: `enchant.InputSource`
+     * Issued object: {@link enchant.InputSource}
      */
     static INPUT_STATE_CHANGED = 'inputstatechanged'
 
     /**
      * An event dispatched when the 'left' button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static LEFT_BUTTON_DOWN = 'leftbuttondown'
 
     /**
      * An event dispatched when the 'left' button is released.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static LEFT_BUTTON_UP = 'leftbuttonup'
 
     /**
      * An event dispatched when the 'right' button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static RIGHT_BUTTON_DOWN = 'rightbuttondown'
 
     /**
      * An event dispatched when the 'right' button is released.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static RIGHT_BUTTON_UP = 'rightbuttonup'
 
     /**
      * An event dispatched when the 'up' button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static UP_BUTTON_DOWN = 'upbuttondown'
 
     /**
      * An event dispatched when the 'up' button is released.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static UP_BUTTON_UP = 'upbuttonup'
 
     /**
      * An event dispatched when the 'down' button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static DOWN_BUTTON_DOWN = 'downbuttondown'
 
     /**
      * An event dispatched when the 'down' button is released.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static DOWN_BUTTON_UP = 'downbuttonup'
 
     /**
      * An event dispatched when the 'a' button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static A_BUTTON_DOWN = 'abuttondown'
 
     /**
      * An event dispatched when the 'a' button is released.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static A_BUTTON_UP = 'abuttonup'
 
     /**
      * An event dispatched when the 'b' button is pressed.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static B_BUTTON_DOWN = 'bbuttondown'
 
     /**
      * An event dispatched when the 'b' button is released.
      * 
-     * Issued by: `enchant.Core`, `enchant.Scene`
+     * Issued by: {@link enchant.Core}, {@link enchant.Scene}
      */
     static B_BUTTON_UP = 'bbuttonup'
 
@@ -367,6 +369,8 @@ export default class Event {
 
     /**
      * An event dispatched to the Timeline when an Action is added.
+     * 
+     * Issued by: {@link enchant.Timeline}
      */
     static ACTION_ADDED = 'actionadded'
 
@@ -378,7 +382,7 @@ export default class Event {
     /**
      * An event dispatched when an animation finishes, meaning null element was encountered
      * 
-     * Issued by: `enchant.Sprite`
+     * Issued by: {@link enchant.Sprite}
      */
     static ANIMATION_END = 'animationend'
 }
