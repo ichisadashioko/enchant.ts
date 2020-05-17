681c6f14292871c6403152f505f26743f69517b7
Ichisada Shioko <ichisadashioko@gmail.com>
2020-05-17 20:57:46 +0700

    Label: initialize properties

--- src/enchant/Label.ts
+++ src/enchant/Label.ts
@@ -3,14 +3,48 @@ import Entity from './Entity'
 export default class Label extends Entity {
 
     _text: string
+    _width: number
+
+    font: string
+    textAlign: string
+
+    _debugColor: string
+    _boundWidth: number
+    _boundHeight: number
+    _boundOffset: number
+    _splitText: Array<{ text: string, width: number, height: number }>
+
+    constructor(text: string) {
+        super()
+
+        // initialize properties
+        // TODO check to see if this may lead to any side effects
+        this._text = ''
+        this._width = 0
+        this._boundWidth = 0
+        this._boundHeight = 0
+        this._boundOffset = 0
+        this._splitText = []
+
+        this.text = text || ''
+        this.width = 300
+        this.font = '14px serif'
+        this.textAlign = 'left'
+
+        this._debugColor = '#ff0000'
+    }
+
     get text(): string {
         return this._text
     }
+
     set text(value: string) {
         value = '' + value
+
         if (this._text === value) {
             return
         }
+
         this._text = value
         value = value.replace(/<br ?\/?>/gi, '<br/>')
         let splitText = value.split('<br/>')
@@ -29,34 +63,14 @@ export default class Label extends Entity {
         }
     }
 
-    _width: number
     get width(): number {
         return this._width
     }
+
     set width(value: number) {
         this._width = value
         this._dirty = true
         this.updateBoundArea()
-    }
-
-    font: string
-    textAlign: string
-
-    _debugColor: string
-    _boundWidth: number
-    _boundHeight: number
-    _boundOffset: number
-    _splitText: Array<{ text: string, width: number, height: number }>
-
-    constructor(text: string) {
-        super()
-
-        this.text = text || ''
-        this.width = 300
-        this.font = '14px serif'
-        this.textAlign = 'left'
-
-        this._debugColor = '#ff0000'
     }
 
     cvsRender() {
@@ -97,6 +111,7 @@ export default class Label extends Entity {
                     div.style[prop] = this._style[prop]
                 }
             }
+
             text = text || this._text
             div.innerHTML = text.replace(/ /g, '&nbsp;')
             div.style.whiteSpace = 'noWrap'
